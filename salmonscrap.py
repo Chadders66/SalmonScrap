a5bec3e26cd07277cd26e4ffac62058159ced29c
Chadders66 <56765555+Chadders66@users.noreply.github.com>
2020-01-07 15:32:04 +0000

    Update salmonscrap.py

--- salmonscrap.py
+++ salmonscrap.py
@@ -1,21 +1,71 @@
-startingMoney = 4000
+import json
 
+startingMoney = 4000
+playerList = []
+
+with open('SalmonScrap/v0.2/boats.json') as json_file:
+    boats = json.load(json_file)
 
 class Player:
     def __init__(self, name):
+        self.pnum = len(playerList)+1
         self.name = name
         self.money = startingMoney
         self.boats = []
         self.staff = 0
+        playerList.append(self)
 
     def printStats(self):
-        print("%s:  Money: %s  Boats: %s  Employees: %s" %(self.name,self.money,self.boats,self.staff))
+        print("Player %d (%s):  Money: %s" %(self.pnum,self.name,self.money))
 
-##class Boat:
+
+class Boat:
+    def __init__(self, name):
+        self.name = 'For Sale'
+        self.type = name
+        self.desc = boats[name]['desc']
+        self.cap = boats[name]['cap']
+        self.size = boats[name]['size']
+        self.cost = boats[name]['cost']
+
+    def buyBoat(self, player, choice, newname):            #add boat to player.boats
+        self.name = newname
+        self.type = choice
+        self.hold = 0
+        self.crew = []
+        self.cost = (boats[choice]['cost'])*0.6
+        self.relB = boats[choice]['relB']
+        self.relI = boats[choice]['relI']
+        self.cspB = boats[choice]['cspB']
+        self.cspI = boats[choice]['cspI']
+        self.loc = "In Harbour"
+        player.money = player.money - boats[choice]['cost']
+        player.boats.append(self)
+        
+
+def initBoats():
+    global fishingBoat
+    global longliner
+    global smallYacht
+    global yacht
+    global smallTrawler
+    global trawler
+    global smallSeine
+    global seineBoat
+    fishingBoat = Boat('Fishing Boat')
+    longliner = Boat('Longliner')
+    smallYacht = Boat('Small Yacht')
+    yacht = Boat('Yacht')
+    smallTrawler = Boat('Small Trawler')
+    trawler = Boat('Trawler')
+    smallSeine = Boat('Small Seine')
+    seineBoat = Boat('Seine Boat')
+
+initBoats()
 
 player1 = Player("Andrew")
 
-player2 = Player("Chadders")
-
 player1.printStats()
-player2.printStats()
+
+fishingBoat.buyBoat(player1, 'Fishing Boat', 'H.M.S. Riven')
+
