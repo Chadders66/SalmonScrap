47149b4411ec14bec400de9bc6eb0a2e96fed5c8
Chadders66 <jchad666@gmail.com>
2020-01-10 13:51:16 +0000

    buttons added and tested

--- salmonscrap.py
+++ salmonscrap.py
@@ -2,8 +2,9 @@ from tkinter import *
 from PIL import ImageTk,Image
 import json
 import os
+import time
 
-path = 'SalmonScrap/GUI/SalmonScrap/boats.json'
+path = ''
 
 startingMoney = 4000
 playerList = []
@@ -11,13 +12,19 @@ forSaleBoats = []
 forSaleNames = ['Fishing Boat', 'Longliner', 'Small Yacht', 'Yacht', 'Small Trawler', 'Trawler', 'Small Seine', 'Seine Boat']
 imageList = []
 
-help1 = [0, 0, 2, 2]
-help2 = [0, 2, 0, 2]
+help1 = [0, 0, 1, 1]
+help2 = [0, 1, 0, 1]
 
-help3 = [0, 0, 1, 1]
-help4 = [0, 1, 0, 1]
+help3 = [0, 0, 3, 3]
+help4 = [0, 4, 0, 4]
 
-with open(path) as json_file:
+help5 = [1, 1, 1, 2, 2, 2]
+help6 = [0, 1, 2, 0, 1, 2]
+
+help7 = ['W', '', 'E', 'W', '', 'E']
+help8 = []
+
+with open(path + 'boats.json') as json_file:
     boats = json.load(json_file)
 
 root = Tk()
@@ -30,30 +37,58 @@ def initBoats():
 
 def resizeImages():
     for x in range(len(forSaleNames)):
-        imageList.append(Image.open("SalmonScrap/v0.2/assets/" + forSaleBoats[x].image)) #resize image
+        imageList.append(Image.open(path + "assets/" + forSaleBoats[x].image)) #resize image
         imageList[x] = imageList[x].resize((165, 130), Image.ANTIALIAS)
-    imageList.append(Image.open("SalmonScrap/v0.2/assets/blank.png"))
+    imageList.append(Image.open(path + "assets/blank.png"))
     imageList[8] = imageList[8].resize((165, 130), Image.ANTIALIAS)
+
+def hirepop():
+    print('Bloop')
+
+def launchpop():
+    print('Bloop')
+
+def renamepop():
+    print('Bloop')
+
+def firepop():
+    print('Bloop')
+
+def sellpop():
+    print('Bloop')
+
+def repairpop():
+    print('Bloop')
 
 def populateFrames():
     img_blank = ImageTk.PhotoImage(imageList[8])
-    m1 = 0
-    m2 = 0
-    if len(playerList) == 2:
-        m2 += 2
-    elif len(playerList) == 3:
-        m1 += 2
-    elif len(playerList) == 4:
-        m1 += 2
-        m2 += 2
-    elif len(playerList) > 4:
+    if len(playerList) > 4:
         print('Too many players!')
         exit()
     for x in range(len(playerList)):
         for y in range(4):
             playerList[x].labels.append(Label(playerList[x].frame, image=img_blank))
             playerList[x].labels[y].image = img_blank
-            playerList[x].labels[y].grid(row=(help3[y]+m1), column=(help4[y]+m2), padx=10, pady=5)
+            playerList[x].labels[y].grid(row=(help3[y]), column=(help4[y]), columnspan=3)
+            buttonList = []
+            for z in range(6):
+                shipButtons = ['Hire', 'Launch', 'Rename', 'Fire', 'Sell', 'Repair']
+                shipCommands = [hirepop, launchpop, renamepop, firepop, sellpop, repairpop]
+                buttonList.append(Button(playerList[x].frame, text=shipButtons[z], command=shipCommands[z], width=6))
+                if y == 0:
+                    n1 = 0
+                    n2 = 0   
+                elif y == 1:
+                    n1 = 0
+                    n2 = 4
+                elif y == 2:
+                    n1 = 3
+                    n2 = 0
+                elif y == 3:
+                    n1 = 3
+                    n2 = 4
+                buttonList[z].grid(row=(help5[z]+n1), column=(help6[z]+n2), sticky=help7[z])
+                playerList[x].buttons.append(buttonList)
 
 class Player:
     def __init__(self, name):
@@ -64,10 +99,11 @@ class Player:
         self.staff = []
         self.frame = []
         self.labels = []
-        currentPlayers = len(playerList)
+        self.buttons = []
+        currentPs = len(playerList)
         playerList.append(self)
-        self.frame = LabelFrame(root, text=name, padx=20, pady=20)
-        self.frame.grid(row=help1[currentPlayers], column=help2[currentPlayers], rowspan=2, columnspan=2, padx=10, pady=5)
+        self.frame = LabelFrame(root, text=name)
+        self.frame.grid(row=help1[currentPs], column=help2[currentPs], rowspan=1, columnspan=1)
 
     def printStats(self):
         print("Player %d (%s):  Money: %s" %(self.pnum,self.name,self.money))
@@ -119,6 +155,10 @@ class Boat:
         player.boats[currentboats].loc = "In Harbour"
         player.boats[currentboats].player = player
         player.boats[currentboats].image = boats[choice]['image']
+        imageIndex = forSaleNames.index(choice)
+        boat_img = ImageTk.PhotoImage(imageList[imageIndex])
+        player.labels[currentboats].configure(image=boat_img)
+        player.labels[currentboats].image = boat_img
             
     def hire(self, player, boat, rank, hirereq):
         if len(self.crew) == 0:
@@ -142,5 +182,11 @@ player1 = Player("Andrew")
 player2 = Player("Chadders")
 
 populateFrames()
+
+selection = 0
+forSaleBoats[selection].buyBoat(player1, forSaleNames[selection], 'H.M.S. Riven')
+selection = 3
+forSaleBoats[selection].buyBoat(player2, forSaleNames[selection], 'H.M.S. Sleepy')
+
 
 root.mainloop()
